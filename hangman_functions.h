a0d7ce4b3a4c1dbb2ea4fd8188cad1fa1b5c38c7
kritika123 <133243247+jain-kritika@users.noreply.github.com>
2024-02-29 22:54:03 +0530

    Add files via upload

---
+++ hangman_functions.h
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void greet();
+void display_misses(int misses);
+void display_status(std::vector<char> incorrect , string answer);
+void end_game(string answer , string codeword);
